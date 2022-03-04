28e587991c03227fa3ea241dae3f6a704ca9692d
simranbcss <62022022+simranbcss@users.noreply.github.com>
2022-03-04 20:00:33 +0500

    Update dboperations.js
    Added Deleting Function and inserting function

--- dboperations.js
+++ dboperations.js
@@ -21,3 +21,42 @@ async function getData(res, query) {
         })
     });
   }
+//to add the data in particular table
+  async function addData(res, params, tableName) {   
+    pool.getConnection((err, connection) => {
+        connection.query('INSERT INTO '+tableName+' SET ?', params, (err, rows) => {
+        connection.release() // return the connection to pool
+
+        if (!err) {
+            res.status(200).json(" with the record ID  has been added.");
+        } else {
+            res.status(400).json(err);
+        }
+        console.log('The data from  table are: \n', rows)
+    })
+});
+
+}
+//For Deleting the data from table with specific id
+async function DeleteData(res, id, tableName) {   
+ 
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        connection.query('DELETE FROM '+tableName+' WHERE id = ?', [id], (err, rows) => {
+            connection.release() // return the connection to pool
+            if (!err) {
+                res.status(200).send('Deleted!');
+            } else {
+                res.status(400).json(err);
+            }
+            
+            console.log('The data from  table are: \n', rows)
+        })
+    });
+
+}
+// exporting the modules
+  module.exports={
+    getData:getData,
+    //getOrder:getOrder
+  }
