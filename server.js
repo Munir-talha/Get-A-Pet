1e5b711d86ed69dcc69297f88d9e91784eeecd65
Sheikh Abdul Wahid <sheikhabdul.bcss19@iba-suk.edu.pk>
2022-03-06 15:40:19 +0500

    Category Func Added

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect(URI, {
 
 
 app.use('/user',require('./routes/userRouter'));
-
+app.use('/api', require('./routes/categoryRouter'))
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () =>{
     console.log('Server is running on port', PORT)
